c679d6cabc275fed363206b839a7c34a7433399c
MohammadAli Arjomand <ma.arjomand1387@gmail.com>
2023-09-05 18:47:23 +0330

    SearchBox Mobile bug fixed

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,32 +1,37 @@
 <script setup>
-    import router from "@/router";
-    const props = defineProps(["text", "refresh"])
-    function search(e) {
-        if (e.code === "Enter") {
-            router.push(`/search/${e.target.value}`)
-                .then(() => { if (props.refresh) location.reload() })
-        }
-    }
+import router from "@/router";
+
+const props = defineProps(["text", "refresh"])
+
+function search() {
+    router.push(`/search/${document.getElementById("searchText").value}`)
+        .then(() => {
+            if (props.refresh) location.reload()
+        })
+}
 </script>
 
 <template>
-    <div>
-        <input type="text" @keydown="search" placeholder="چی میخوای ببینی؟" :value="props.text">
-    </div>
+    <form @submit.prevent="search">
+        <div>
+            <input type="text" id="searchText" placeholder="چی میخوای ببینی؟" :value="props.text">
+        </div>
+    </form>
 </template>
 
 <style scoped>
-    div {
-        width: 80vw;
-        background: white;
-        box-shadow: #ddd 0 0 4px 1px;
-        border-radius: 10px;
-        padding: 12px;
-    }
-    input {
-        width: 100%;
-        background: transparent;
-        border: none;
-        outline: none;
-    }
+div {
+    width: 80vw;
+    background: white;
+    box-shadow: #ddd 0 0 4px 1px;
+    border-radius: 10px;
+    padding: 12px;
+}
+
+input {
+    width: 100%;
+    background: transparent;
+    border: none;
+    outline: none;
+}
 </style>
