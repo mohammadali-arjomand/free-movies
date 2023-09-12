2500716fdd07dd73dac68d1f0081b2e956c47b45
MohammadAli Arjomand <ma.arjomand1387@gmail.com>
2023-09-12 12:51:13 +0330

    Adding vuetify

--- src/main.js
+++ src/main.js
@@ -3,6 +3,18 @@ import './assets/main.css'
 import { createApp } from 'vue/dist/vue.esm-bundler.js'
 import router from "@/router";
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
 createApp({})
     .use(router)
+    .use(vuetify)
     .mount('#app')
