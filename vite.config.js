cc90fef79de1b3e9c0d7d4574a1254252722aae2
MohammadAli Arjomand <ma.arjomand1387@gmail.com>
2023-09-19 15:49:16 +0330

    became pwa

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,43 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+        manifest: {
+            "name": "Free Movie",
+            "short_name": "Free Movie",
+            "start_url": "/",
+            "display": "standalone",
+            "background_color": "#ffffff",
+            "theme_color": "#000000",
+            "description": "دانلود و تماشای فیلم و سریال",
+            "icons": [
+                {
+                    "src": "icons/icon-192.png",
+                    "sizes": "192x192",
+                    "type": "image/png"
+                },
+                {
+                    "src": "icons/icon-512.png",
+                    "sizes": "512x512",
+                    "type": "image/png"
+                }
+            ],
+            "related_applications": [],
+            "prefer_related_applications": false
+        },
+        includeAssets: [
+            'favicon.ico',
+            'icons/icon-192.png',
+            'icons/icon-512.png'
+        ],
+        mode: 'development'
+    })
   ],
   resolve: {
     alias: {
